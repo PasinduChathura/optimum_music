7bd272895d5705ad24ee1d663ba8064e6e0aedfa
PasinduChathura <71862854+PasinduChathura@users.noreply.github.com>
2021-10-11 18:01:41 +0530

    edited home screen

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -10,32 +10,13 @@ class AppBarWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final _size = MediaQuery.of(context).size;
     return Container(
-      // height: _size.height * 0.5,
       color: HexColor('0a1227'),
       child: Padding(
-        padding:  EdgeInsets.only(top: _size.height * 0.06),
+        padding: EdgeInsets.only(top: _size.height * 0.06),
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: _size.width * 0.05),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: SvgIcon(
-                  'menu_icon.svg',
-                  height: 10,
-                ),
-              ),
-              AppLogo(
-                height: _size.height * 0.09,
-              ),
-              Icon(
-                Icons.search,
-                color: Colors.white,
-                size: _size.width * 0.07,
-              )
-            ],
+          child: AppLogo(
+            height: _size.height * 0.09,
           ),
         ),
       ),
