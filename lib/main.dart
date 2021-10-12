6232f967386967aae1d8b263700d7be890190383
PasinduChathura <71862854+PasinduChathura@users.noreply.github.com>
2021-10-12 11:38:20 +0530

    app icon and app name changed

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
         theme: CustomTheme.lightTheme,
         home: const SplashScreen());
   }
